f877d032da1b767877a107c28a14a6780be58076
jx1 <jx1@email.com>
2017-08-31 14:35:33 +0800

    communicate

---
+++ src/components/child.vue
@@ -0,0 +1,45 @@
+<template>
+	<div>
+		<ul>
+			<li>{{msg}}</li>
+			<li>{{childMsg}}</li>
+			<li>
+				<div @click="toParent">click To parent</div>
+			</li>			
+		</ul>
+	</div>
+</template>
+<script>
+export default {
+	name:"child",
+	components: {
+		
+	},
+	data () {
+	    return {
+	      msg: 'this is child',
+	      data:"from child"
+	    }
+	},
+	props:{
+		childMsg: {
+	        type: String,
+	        default: "null" //这样可以指定默认的值
+   		}
+	},
+	methods:{
+		toParent(){
+			this.$emit('upup',this.data); //主动触发upup方法，'hehe'为向父组件传递的数据
+		}
+	}
+}
+</script>
+
+<style scoped>
+li{
+	list-style: none;
+}
+ul{
+	background: #CCCCCC;
+}
+</style>
---
+++ src/components/main.vue
@@ -0,0 +1,31 @@
+<template>
+	<div>
+		<child @upup="change" :child-msg="data"></child>
+		<div>
+		    {{childData}}
+		</div>
+	</div>
+</template>
+<script>
+import child from './child.vue';
+export default {
+	components: {
+		child
+	},
+	data () {
+	    return {
+	       	data: "from parent",
+	       	childData:""
+	    }
+	},
+	methods:{
+		change(msg){
+			this.childData=msg;
+			this.data="parent change"
+		}
+	}
+}
+</script>
+
+<style scoped>
+</style>
